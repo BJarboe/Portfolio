d28f83cd400989b98d3d1c3ceee0ce786db21f40
BJarboe <115917843+BJarboe@users.noreply.github.com>
2022-10-16 18:15:51 -0700

    Add files via upload

---
+++ promotedCarModelStack.cpp
@@ -0,0 +1,61 @@
+#include "promotedCarModelStack.h"
+
+/**
+   * @brief push operation, pushing the latest promoted model onto the stack
+            Both time and auxiliary space complexity need to be O(1) 
+   * @param model 
+   * @param price 
+   */
+void PromotedCarModelStack::push(string model, int price) {
+  // Write your code
+}
+
+/**
+   * @brief pop operation, popping the latest promoted model off the stack
+            Both time and auxiliary space complexity need to be O(1) 
+   * @param  
+   * @return PromotedModel
+   */
+PromotedModel PromotedCarModelStack::pop() {
+  // Write your code
+
+  return PromotedModel();
+}
+
+/**
+   * @brief peek operation, peeking the latest promoted model at the top of the stack (without popping)
+            Both time and auxiliary space complexity need to be O(1) 
+   * @param 
+   * @return PromotedModel
+   */
+PromotedModel PromotedCarModelStack::peek() {
+  // Write your code
+
+  return PromotedModel();
+}
+
+/**
+   * @brief getHighestPricedPromotedModel, 
+   *        getting the highest priced model among the past promoted models
+            Both time and auxiliary space complexity need to be O(1)
+   * @param 
+   * @return PromotedModel
+   */
+PromotedModel PromotedCarModelStack::getHighestPricedPromotedModel() {
+  // Write your code
+
+  return PromotedModel();
+}
+
+/**
+   * @brief getLowestPricedPromotedModel, 
+   *        getting the lowest priced model among the past promoted models
+            Both time and auxiliary space complexity need to be O(1)
+   * @param 
+   * @return PromotedModel
+   */
+PromotedModel PromotedCarModelStack::getLowestPricedPromotedModel() {
+  // Write your code
+
+  return PromotedModel();
+}
